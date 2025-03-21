8d63f7ee04bdd8b064e72522e05edc6e239bb42f
naveenjustcoder <naveencomputerengineer@gmail.com>
2025-03-21 18:27:24 +0530

    fix:resolved most of checkov issues

--- create-s3.tf
+++ create-s3.tf
@@ -6,21 +6,22 @@ resource "aws_s3_bucket" "com_div_proj_comp_s3_bucket" {
   bucket_prefix = "com.div.proj.comp"  # Bucket name must be globally unique
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "com_div_proj_comp_s3_bucket-sse" {
-  bucket = aws_s3_bucket.com.div.proj.comp.s3-bucket.id
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_bucket.id
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
 resource "aws_s3_bucket_versioning" "com_div_proj_comp_s3_bucket_versioning" {
-  bucket = aws_s3_bucket.com.div.proj.comp.s3-bucket.id
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
-resource "aws_s3_bucket_lifecycle_configuration" "example" {
-  bucket = aws_s3_bucket.bucket.id
+resource "aws_s3_bucket_lifecycle_configuration" "com_div_proj_comp_s3_bucket_lifecycle" {
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_bucket.id
 
   rule {
     id      = "expire"
@@ -67,13 +68,34 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
 
 # Optional: Create a bucket for logging
 resource "aws_s3_bucket" "com_div_proj_comp_s3_LOG_BUCKET" {
-  bucket = "com.div.proj.comp.s3-log-bucket"  # Replace with a unique bucket name
+  bucket_prefix = "com.div.proj.comp.s3-log-bucket"  # Replace with a unique bucket name
 }
-resource "aws_s3_bucket_acl" "com-div-proj-comp-s3-LOG-BUCKET-acl" {
-  bucket = aws_s3_bucket.com_div_proj_comp_s3_bucket.id
-  acl    = "log-delivery-write"
+resource "aws_s3_bucket_server_side_encryption_configuration" "com_div_proj_comp_s3_LOG_BUCKET-sse" {
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_LOG_BUCKET.id
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
-
+resource "aws_s3_bucket_versioning" "com_div_proj_comp_s3_LOG_BUCKET_versioning" {
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_LOG_BUCKET.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+# resource "aws_s3_bucket_acl" "com_div_proj_comp_s3_LOG_BUCKET-acl" {
+#   bucket = aws_s3_bucket.com_div_proj_comp_s3_bucket.id
+#   acl    = "log-delivery-write"
+# }
+resource "aws_s3_bucket_public_access_block" "com_div_proj_comp_s3_LOG_BUCKET_block_public_access" {
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_LOG_BUCKET.id
+  block_public_acls   = true
+  block_public_policy = true
+  restrict_public_buckets = true
+  ignore_public_acls=true
+}
 resource "aws_s3_bucket_logging" "com_div_proj_comp_s3_bucket_logging" {
   bucket = aws_s3_bucket.com_div_proj_comp_s3_bucket.id
   target_bucket = aws_s3_bucket.com_div_proj_comp_s3_LOG_BUCKET.id
@@ -84,7 +106,7 @@ resource "aws_sns_topic" "com_div_proj_comp_log_bucket_sns_topic" {
   kms_master_key_id = "alias/aws/sns"
 }
 resource "aws_s3_bucket_notification" "log_bucket_notification" {
-  bucket = aws_s3_bucket.com.div.proj.comp.s3-log-bucket.id
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_LOG_BUCKET.id
   topic {
     topic_arn     = aws_sns_topic.com_div_proj_comp_log_bucket_sns_topic.arn
     events        = ["s3:ObjectRemoved:*"]
@@ -92,6 +114,32 @@ resource "aws_s3_bucket_notification" "log_bucket_notification" {
   }
 }
 
+resource "aws_s3_bucket_lifecycle_configuration" "com_div_proj_comp_s3_LOG_BUCKET_lifecycle" {
+  bucket = aws_s3_bucket.com_div_proj_comp_s3_LOG_BUCKET.id
+
+  rule {
+    id      = "expire"
+    status  = "Enabled"
+    filter{
+      prefix = "logs/"
+    }
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    expiration {
+      days = 1000
+    }
+  }
+  rule {
+    id = "failed_uploads"
+    status = "Enabled"
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+    filter {}
+  }
+}
 
 # Outputs for reference
 output "s3_bucket_name" {

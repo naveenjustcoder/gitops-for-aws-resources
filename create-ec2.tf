d71d445bed9f292ef3e13a117d45de866c0d925b
naveenjustcoder <naveencomputerengineer@gmail.com>
2025-03-21 19:46:09 +0530

    fix: replaced datasource block with output block to get public ip

--- create-ec2.tf
+++ create-ec2.tf
@@ -58,6 +58,7 @@ resource "aws_iam_instance_profile" "my_ec2_instance_profile" {
   role = aws_iam_role.my_ec2_role.name
 }
 
-data "aws_instance" "public_ip" {
-  public_ip = aws_instance.example.public_ip
+output "ec2_public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the EC2 instance"
 }
